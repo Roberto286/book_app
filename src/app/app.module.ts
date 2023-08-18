34ae511471ece1368002cabf366a1b50eec59dc3
Roberto286 <roberto.saliola96@gmail.com>
2023-08-18 17:17:26 +0200

    updated imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { NavbarComponent } from './navbar/navbar.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
-import { GridComponent } from './grid/grid.component';
+import { GridComponent } from './components/grid/grid.component';
 import { MatGridListModule } from '@angular/material/grid-list';
-import { CardComponent } from './card/card.component';
+import { CardComponent } from './components/card/card.component';
 import { MatCardModule } from '@angular/material/card';
 import { MatButtonModule } from '@angular/material/button';
 
@@ -24,6 +25,7 @@ import { MatButtonModule } from '@angular/material/button';
     MatGridListModule,
     MatCardModule,
     MatButtonModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
