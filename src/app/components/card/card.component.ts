d5861949e49ce6ab9605d2df30e1f0d7489d71cf
Roberto286 <roberto.saliola96@gmail.com>
2023-08-19 14:48:31 +0200

    added data binding to card component

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,10 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+
+type Book = {
+  id: string;
+  title: string;
+  author: string;
+  genre: string;
+  price: string;
+  cover?: string;
+};
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
-  styleUrls: ['./card.component.scss']
+  styleUrls: ['./card.component.scss'],
 })
 export class CardComponent {
+  @Input() data: Book = {
+    id: '',
+    title: '',
+    author: '',
+    genre: '',
+    price: '',
+  };
 
+  constructor() {}
 }
