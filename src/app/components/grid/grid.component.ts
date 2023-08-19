9df1bba2573b405ee31a8fc7c8850ce6d542f93a
Roberto286 <roberto.saliola96@gmail.com>
2023-08-19 14:49:02 +0200

    fetching books from api

--- src/app/components/grid/grid.component.ts
+++ src/app/components/grid/grid.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FetchBooksService } from 'src/app/services/fetchBooks/fetch-books.service';
+
+type Book = {
+  id: string;
+  title: string;
+  author: string;
+  genre: string;
+  price: string;
+  cover?: string;
+};
 
 @Component({
   selector: 'app-grid',
   templateUrl: './grid.component.html',
-  styleUrls: ['./grid.component.scss']
+  styleUrls: ['./grid.component.scss'],
 })
-export class GridComponent {
+export class GridComponent implements OnInit {
+  books: Book[] = [];
 
+  constructor(private _bookService: FetchBooksService) {}
+
+  ngOnInit() {
+    this._bookService
+      .getBooks()
+      .subscribe((res) => (this.books = res as Book[]));
+  }
 }
